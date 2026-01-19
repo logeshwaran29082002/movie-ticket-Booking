6153f31f29002f6407b3a19a7dba93fbe9babeb5
Logeshwaran <logurashitha2002@gmail.com>
2026-01-19 17:29:04 +0530

    dashboard finesh

---
+++ admin/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import DashboardPage from '../components/DashboardPage'
+
+function Dashboard() {
+  return (
+    <div>
+      <Navbar/>
+      <DashboardPage/>
+    </div>
+  )
+}
+
+export default Dashboard
