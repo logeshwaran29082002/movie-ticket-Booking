fd9751c39e3d5df90b4fe94b22152d7d174b2b1a
Logeshwaran <logurashitha2002@gmail.com>
2026-01-30 17:39:12 +0530

    change routes

--- backend/src/routes/movieRoutes.js
+++ backend/src/routes/movieRoutes.js
@@ -1,10 +1,32 @@
 const express = require("express");
-const { createMovie, getMovies, getMovieById, deleteMovie } = require("../controller/movieController");
+const {
+  createMovie,
+  getMovies,
+  getMovieById,
+  deleteMovie
+} = require("../controller/movieController");
+
 const upload = require("../utils/multer");
 
 const movieRouter = express.Router();
 
-movieRouter.post("/", upload.single("poster"), createMovie);
+movieRouter.post(
+  "/",
+  upload.fields([
+    { name: "poster", maxCount: 1 },
+
+    { name: "castFiles", maxCount: 20 },
+    { name: "directorFiles", maxCount: 20 },
+    { name: "producerFiles", maxCount: 20 },
+
+    { name: "ltThumbnail", maxCount: 1 },
+    { name: "ltDirectorFiles", maxCount: 20 },
+    { name: "ltProducerFiles", maxCount: 20 },
+    { name: "ltSingerFiles", maxCount: 20 }
+  ]),
+  createMovie
+);
+
 movieRouter.get("/", getMovies);
 movieRouter.get("/:id", getMovieById);
 movieRouter.delete("/:id", deleteMovie);
