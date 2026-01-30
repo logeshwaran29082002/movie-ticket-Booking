fd347bd2cde387efe3bd7655b34a03bbe559664c
Logeshwaran <logurashitha2002@gmail.com>
2026-01-30 16:52:17 +0530

    change the code

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,6 +1,6 @@
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary");
 
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
--- backend/src/utils/multer.js
+++ backend/src/utils/multer.js
@@ -1,15 +1,10 @@
 const multer = require("multer");
-const CloudinaryStorage = require("multer-storage-cloudinary");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("./cloudinary");
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "movies",
-    public_id: (req, file) => Date.now() + "-" + file.originalname,
-  },
+  cloudinary: cloudinary.v2,
+  params: { folder: "movies" },
 });
 
-const upload = multer({ storage });
-
-module.exports = upload;
+module.exports = multer({ storage });
